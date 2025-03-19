9e2b016a11807c8f5267d9a0a60c16367afbf14d
Javier Zavala <javierzavalatorres@gmail.com>
2025-03-19 19:43:45 -0300

    chore: config firebase emulator

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,35 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { connectAuthEmulator, getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  connectFirestoreEmulator,
+  getFirestore,
+  provideFirestore,
+} from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => {
+      const firestore = getFirestore();
+      if (environment.useEmulators) {
+        connectFirestoreEmulator(firestore, 'localhost', 8080);
+      }
+      return firestore;
+    }),
+    provideAuth(() => {
+      const auth = getAuth();
+      if (environment.useEmulators) {
+        connectAuthEmulator(auth, 'http://localhost:9099', {
+          disableWarnings: true,
+        })
+      }
+      return auth;
+    }),
+  ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: "tortugagris-url-shortener",
+    appId: "1:21377519266:web:703774ca3623dafec991fb",
+    storageBucket: "tortugagris-url-shortener.firebasestorage.app",
+    apiKey: "AIzaSyDwHIvDlvb05DCQwtG57dtcVH1rVD-jMj0",
+    authDomain: "tortugagris-url-shortener.firebaseapp.com",
+    messagingSenderId: "21377519266"
+  },
+  useEmulators: false,
+  production: true,
+};
