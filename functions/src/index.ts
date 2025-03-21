3706f8fedcda90c63d0c159aa4e9c0b1102ff452
Javier Zavala <javierzavalatorres@gmail.com>
2025-03-21 18:04:03 -0300

    feat: add chrone to delete expired links

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,8 +7,10 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 import {onRequest} from "firebase-functions/v2/https";
-import API from "./api/app";
+import {onSchedule} from "firebase-functions/scheduler";
 import * as admin from "firebase-admin";
+import API from "./api/app";
+import {Timestamp} from "firebase-admin/firestore";
 admin.initializeApp();
 // import * as logger from "firebase-functions/logger";
 
@@ -20,3 +22,23 @@ admin.initializeApp();
 //   response.send("Hello from Firebase!");
 // });
 exports.api = onRequest(API);
+
+export const deleteExpiredLinks = onSchedule(
+  "every day 00:00",
+  async (context) => {
+    const linksRef = admin.firestore().collection("links");
+    const currentTime = Timestamp.now();
+
+    // Fetch expired links
+    const snapshot = await linksRef
+      .where("expiredAt", "<=", currentTime)
+      .get();
+
+    // Delete expired links
+    snapshot.forEach(async (doc) => {
+      await doc.ref.delete();
+    });
+
+    console.log(`Deleted ${snapshot.size} expired links.`);
+  }
+);
